a386a2d083173733a175c37c18f2156a696180ab
Li184805054 <2173808397@qq.com>
2019-09-09 23:01:34 +0800

    999

---
+++ Oushusum.java
@@ -0,0 +1,11 @@
+public class Oushusum{
+      public static void main(String[] args){
+      int sum=0;
+      for(int i=1;i<=10;i++){
+      if(i%2!=0){
+      continue;}
+      sum=sum+i;
+}
+     System.out.println("sum="+sum);
+}
+}
